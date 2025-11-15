e8a345f1990fee6df977c701881c97f73c39834b
Andrea Duarte <168836196+andreajgd@users.noreply.github.com>
2025-11-15 15:28:41 -0600

    add clase HistorialEstadoPedido

--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/pedidos/HistorialEstadoPedido.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/pedidos/HistorialEstadoPedido.java
@@ -1,4 +1,39 @@
 package uam.edu.ni.KathEncargos.domain.pedidos;
 
+import lombok.Getter;
+import lombok.Setter;
+import uam.edu.ni.KathEncargos.domain.catalogos.EstadoPedido;
+import uam.edu.ni.KathEncargos.domain.seguridad.Usuario;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "HistorialEstadoPedido")
+@Getter @Setter
+
 public class HistorialEstadoPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_historial", nullable = false)
+    private Long idHistorial;
+
+    @Column(name = "fecha_hora", nullable = false)
+    private LocalDateTime fechaHora;
+
+    @Column(name = "observacion", length = 300)
+    private String observacion;
+
+    @ManyToOne
+    @JoinColumn(name = "Pedido_id_Pedido", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "EstadoPedido_id_estado", nullable = false)
+    private EstadoPedido estadoPedido;
+
+    @ManyToOne
+    @JoinColumn(name = "Usuario_id_usuario", nullable = false)
+    private Usuario operador;
+
 }
