b4a10e74f8ba3f789f94cffc444d5dd58f5f4015
David Sánchez <164054263+Djave17@users.noreply.github.com>
2025-11-14 21:18:55 -0600

    Categoria Fuert y Categoria Guarnicion

--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/catalogos/CategoriaFuerte.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/catalogos/CategoriaFuerte.java
@@ -1,4 +1,18 @@
 package uam.edu.ni.KathEncargos.domain.catalogos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CategoriaFuerte")
 public class CategoriaFuerte {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_categoriaFuerte", nullable = false)
+    private Long idCategoriaFuerte;
+
+
+
+    @Column(name ="nombre_categoria", length = 100, nullable = false)
+    private String nombreCategoria;
 }
--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/catalogos/CategoriaGuarnicion.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/catalogos/CategoriaGuarnicion.java
@@ -1,4 +1,16 @@
 package uam.edu.ni.KathEncargos.domain.catalogos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CategoriaGuarnicion")
 public class CategoriaGuarnicion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_categoriaGuarnicion", nullable = false)
+    private Long idCategoriaGuarnicion;
+
+    @Column(name ="nombre_categoria", length = 100, nullable = false)
+    private String nombreCategoria;
 }
