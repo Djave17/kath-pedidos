6ab5bada8b998dfd4736092d738a8fdb458e9453
Andrea Duarte <168836196+andreajgd@users.noreply.github.com>
2025-11-15 15:12:44 -0600

    add clase DetallePedido

--- KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/pedidos/DetallePedido.java
+++ KathEncargos/src/main/java/uam/edu/ni/KathEncargos/domain/pedidos/DetallePedido.java
@@ -1,4 +1,41 @@
 package uam.edu.ni.KathEncargos.domain.pedidos;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Money;
+import uam.edu.ni.KathEncargos.domain.menu.Platillo;
+import uam.edu.ni.KathEncargos.domain.seguridad.Usuario;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "DetallePedido")
+@Getter @Setter
+
 public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_detallePedido", nullable = false)
+    private Long idDetallePedido;
+
+    @Column(name  = "cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Money
+    @Column(name= "precio_unitario", nullable = false)
+    private BigDecimal precioUnitario;
+
+    @Money
+    @Column(name = "subtotal", nullable = false)
+    private BigDecimal subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "Platillo_id_platillo", nullable = false)
+    private Platillo platillo;
+
+    @ManyToOne
+    @JoinColumn(name = "Pedido_id_pedido", nullable = false)
+    private Pedido pedido;
+
 }
